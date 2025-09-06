eafcc03c09094c8f6cb07f72438aed98f0cfb773
gingerknight <ryanrichmond6@gmail.com>
2025-09-05 19:48:08 -0600

    Initial repl testing

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,10 +1,10 @@
 // prettier.config.mjs
 export default {
-    tabWidth: 4, // match your Neovim config
-    useTabs: false,
-    semi: true,
-    singleQuote: false,
-    trailingComma: "all",
-    printWidth: 120, // match your textwidth
-    endOfLine: "lf",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 120, // match your textwidth
+  endOfLine: "lf",
 };
--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,27 @@
+import * as readline from "node:readline";
+
 export function cleanInput(input: string): string[] {
-    // Split on whitespace, then filter on truthy values (drop whitespace)
-    const lowerInput = input.toLowerCase();
-    const values: string[] = lowerInput.split(" ").filter((val) => val);
-    return values;
+  // Split on whitespace, then filter on truthy values (drop whitespace)
+  const lowerInput = input.toLowerCase();
+  const values: string[] = lowerInput.split(" ").filter((val) => val);
+  return values;
+}
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "(╯°□°)╯︵◓ >>",
+  });
+  rl.prompt();
+  rl.on("line", (input) => {
+    // define the callback inline instead of seperate func definition
+    // console.log("We got an input capt: ", input);
+    if (!input) {
+      // empty input
+      rl.prompt();
+    }
+    const cleanedInput = cleanInput(input);
+    console.log("Your command was:", cleanedInput[0]);
+    rl.prompt();
+  });
 }
